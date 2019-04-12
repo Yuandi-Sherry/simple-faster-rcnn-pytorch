a5e9796d56b9cedd0e8b5a8acc8d0144a1f055a4
unknown <1094050886@qq.com>
2019-04-12 13:52:25 +0800

    change nms build.py

--- model/utils/nms/build.py
+++ model/utils/nms/build.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
+ext_modules = [Extension("_nms_gpu_post", ["_nms_gpu_post.pyx"],
+                  include_dirs=[numpy.get_include()])]
 ext_modules = [Extension("_nms_gpu_post", ["_nms_gpu_post.pyx"])]
 setup(
     name="Hello pyx",
